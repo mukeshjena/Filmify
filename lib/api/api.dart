4c65393d09c1a5044abdf5dd5932afc0f7b72f85
Mukesh Jena <122939493+mukeshjena@users.noreply.github.com>
2023-09-28 15:21:07 +0530

    Update api.dart

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,5 +1,5 @@
 class Api {
   static const baseUrl = "https://api.themoviedb.org/3/";
   static const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
-  static const apiKey = "faf0ebc01176be7ef6706c8cd53858aa";
+  static const apiKey = "YOUR_OWN_API_KEY";
 }
